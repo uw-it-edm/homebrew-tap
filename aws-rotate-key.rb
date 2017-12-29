0f301589fb411bb0afc179ec472fb9e2d37fd4f5
Maxime Deravet <maximede@users.noreply.github.com>
2017-12-29 11:34:30 -0800

    update to version 1.0.1

--- aws-rotate-key.rb
+++ aws-rotate-key.rb
@@ -1,8 +1,8 @@
 class AwsRotateKey < Formula
   desc "Easily rotate your AWS key."
   homepage "https://github.com/uw-it-edm/aws-rotate-key"
-  url "https://github.com/uw-it-edm/aws-rotate-key/releases/download/v1.0.0/aws-rotate-key-1.0.0-darwin_amd64.zip"
-  sha256 "635882b25a13d893d980eb0b90dcd3623c106ac10ec5926070765686f20f9429"
+  url "https://github.com/uw-it-edm/aws-rotate-key/releases/download/v1.0.1/aws-rotate-key-1.0.1-darwin_amd64.zip"
+  sha256 "dea664e1b4b5654f723220c11ebb8712c3cc323b0dda470b13970bc004761238"
 
   def install
     bin.install "aws-rotate-key"
